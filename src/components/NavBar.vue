e15aa364bb6c3f41f910e887f8598c520e74debc
GerryNdungu <gerryjerry36@gmail.com>
2022-05-23 16:29:44 +0300

    1st Commit

---
+++ src/components/NavBar.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-app-bar app color="red" dark flat class="px-8">
+        <v-btn icon>
+            <v-icon color="#A71210">fas fa-dragon</v-icon>
+        </v-btn>
+    </v-app-bar>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <v-app style="{background: $vuetify.theme.themes.dark.background}">
+    <NavBar />
+      </v-app>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import NavBar from '@/components/NavBar.vue'
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  export default {
+    name: 'HomeView',
+
+    components: {
+      NavBar,
+    },
+  
+        NavBar}
 </script>
