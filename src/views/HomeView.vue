39926ae87cf1fb85f35d3423e307926a67606d9d
GerryNdungu <gerryjerry36@gmail.com>
2022-06-03 12:37:10 +0300

    additions to home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -113,8 +113,107 @@
         </v-col>
         <v-col md="3" offset-md="3">
           <v-card tile class="pa-2 py-12" outlined height="250px" color="#1e1e1e">
-            <v-img src="1.png"></v-img>
+            <v-img src="1.png" contain></v-img>
           </v-card>
+        </v-col>
+        <v-col md="3">
+          <v-card tile class="pa-2 py-12" outlined height="250px" color="#1e1e1e">
+            <v-img src="1.png" contain></v-img>
+          </v-card>
+        </v-col>
+        <v-col md="3" offset-md="3">
+          <v-btn color="white" dark text class="ml-n4"> BUGTRAQ </v-btn
+          ><br />
+
+          <v-btn color="#A7121D" dark text class="ml-n4">
+            know more
+            <v-icon right>mdi-arrow-right</v-icon>
+          </v-btn>
+        </v-col>
+        <v-col md="3">
+          <v-btn color="white" dark text class="ml-n4"> WATER BILLING SYSTEM </v-btn><br />
+
+          <v-btn color="#A7121D" dark text class="ml-n4">
+            know more
+            <v-icon right>mdi-arrow-right</v-icon>
+          </v-btn>
+        </v-col>
+        <v-col md="3" offset-md="3">
+          <v-card
+            class="pa-2 py-12"
+            outlined
+            tile
+            height="250px"
+            color="#1E1E1E"
+          >
+            <v-img src="1.png" contain max-height=""></v-img>
+          </v-card>
+        </v-col>
+        <v-col md="3">
+          <v-card
+            class="pa-2 py-12"
+            outlined
+            tile
+            height="250px"
+            color="#1E1E1E"
+          >
+            <v-img src="1.png" contain max-height=""></v-img>
+          </v-card>
+        </v-col>
+        <v-col md="3" offset-md="3">
+          <v-btn color="white" dark text class="ml-n4"> AAE IdeaPro </v-btn
+          ><br />
+
+          <v-btn color="#A7121D" dark text class="ml-n4">
+            know more
+            <v-icon right>mdi-arrow-right</v-icon>
+          </v-btn>
+        </v-col>
+        <v-col md="3">
+          <v-btn color="white" dark text class="ml-n4"> Mozaik </v-btn><br />
+          <v-btn color="#A7121D" dark text class="ml-n4">
+            know more
+            <v-icon right>mdi-arrow-right</v-icon>
+          </v-btn>
+        </v-col>
+        <v-col cols="12" class="padd topInverse">
+          <div class="second">
+            <div class="secondchild1" id="contact">
+              <v-row>
+                <v-col cols="7">
+                  <h1 class="white--text">
+                    Let's work together on<br />your next project
+                  </h1>
+                  <p class="grey--text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Eius nesciunt ducimus natus <br />
+                  </p>
+                </v-col>
+                <v-col cols="5">
+                  <v-btn tile color="#A7121D" dark class="mt-16">
+                    Contact
+                  </v-btn>
+                </v-col>
+              </v-row>
+            </div>
+          </div>
+          <v-toolbar class="topTolbar" color="#111111" dark flat>
+            <div
+              style="
+                position: absolute;
+                margin-left: auto;
+                margin-right: auto;
+                left: 0;
+                right: 0;
+                text-align: center;
+              "
+            >
+              <v-btn text>Home</v-btn>
+              <v-btn text>Project</v-btn>
+              <v-btn text>about</v-btn>
+              <v-btn text> contact</v-btn>
+            </div>
+          </v-toolbar>
         </v-col>
       </v-row>
     </v-container>
